8c0f12c848d630ddbb613e0224fe6b32338b1d2e
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-07-03 00:02:53 -0300

    Iniciando as Classes

---
+++ contato.class.php
@@ -0,0 +1,52 @@
+<?php
+class Contato {
+	private $pdo;
+
+	public function __construct() {
+		$this->pdo = new PDO("mysql:dbname=crud_oo;host=localhost", "mateus", "");
+	}
+
+	public function adicionar($email, $nome) {
+		if($this->existeEmail($email) == false) {
+			$sql = "INSERT INTO contatos (nome, email) VALUES (:nome, :email)";
+			$sql = $this->pdo->prepare($sql);
+			$sql->bindValue(":nome", $nome);
+			$sql->bindValue(":email", $email);
+			$sql->execute();
+
+			return true;
+		}	else {
+			return false;
+		}
+	}
+
+	public function getNome($email) {
+		$sql = "SELECT nome FROM contatos WHERE email = :email";
+		$sql = $this->pdo->prepare($sql);
+		$sql->bindValue(":email", $email);
+		$sql->execute();
+
+		if($sql->rowCount() > 0) {
+			$info = $sql->fetch();
+			return $info['nome'];
+		} else {
+			return '';
+		}
+	}
+
+	public function getAll() {
+		$sql = "SELECT * FROM contatos";
+		$sql = $this->pdo->query($sql);
+
+		if($sql->rowCount() > 0) {
+			return $sql->fetchAll();
+		} else {
+			array();
+		}
+	}
+
+	public function existeEmail() {
+
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include 'contato.class.php';
+
+$contato = new Contato();
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	
+</body>
+</html>
