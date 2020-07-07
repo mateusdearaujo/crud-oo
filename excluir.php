9d87574e73465ac277b33f507df8d55a0953fce1
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-07-06 23:28:38 -0300

    Criando página para excluir usuário

---
+++ excluir.php
@@ -0,0 +1,12 @@
+<?php
+include 'contato.class.php';
+$contato = new Contato();
+
+if(!empty($_GET['id'])) {
+
+	$id = $_GET['id'];
+
+	$contato->excluir($id);
+} 
+	header("Location: index.php");
+?>
